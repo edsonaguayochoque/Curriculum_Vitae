f3e23fce0fe60d07d91a1bd65ce940009ae4419a
Edson Aguayo Choque <eaguayo@prodemffp.net>
2021-05-13 10:19:53 -0400

    se termino hasta el footer

--- script.js
+++ script.js
@@ -15,3 +15,35 @@
     $menu.classList.remove("is-active");
   });
 })(document);
+
+/********************contactform******************/ 
+((d)=>{
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+    $form.addEventListener("submit",(e)=>{
+      e.preventDefault();
+      $loader.classList.remove("none");
+      fetch("https://formsubmit.co/ajax/edson4888@gmail.com",{method:"POST",
+      body:new FormData(e.target)
+    })
+    .then(res=>res.ok? res.json():Promise.reject(res))
+    .then(json=>{
+      location.hash="#gracias";
+      $form.reset()
+    })
+    .catch(err=>{
+      console.log(err);
+      let mensaje=err.statusText || "Ocurrio un error al enviar, intenta nuevaamente"
+      $response.querySelector("h3").innerHTML=`Error: ${err.status}:${mensaje}`;
+    }).finally(()=>{
+        $loader.classList.add("none");
+        setTimeout(()=>{
+          location.hash="#close"
+        }
+        ,3000);
+    });
+
+    });
+})(document);
